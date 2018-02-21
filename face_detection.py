4a380030cb5b77892327ac4c1f03400c79a904b0
Tuomas Höyhtyä <tuomas.hoyhtya@gmail.com>
2018-02-21 15:31:57 +0200

    removed unused code

--- face_detection.py
+++ face_detection.py
@@ -34,58 +34,10 @@ def getFaces(imagePath):
 	return face_encodings, face_locations
 
 
-#TODO remove or refactor
-def showImage(file, face_encodings, face_locations, known_face_encodings, known_face_names):
-	image = face_recognition.load_image_file("keios.jpg", mode='RGB')
-	image2 = face_recognition.load_image_file(file, mode='RGB')
-	face_names = []
-	for face_encoding in face_encodings:
-		# See if the face is a match for the known face(s)
-		matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-		name = "Unknown"
-
-		# If a match was found in known_face_encodings, just use the first one.
-		if True in matches:
-			print("Found match 8)")
-			first_match_index = matches.index(True)
-			name = known_face_names[first_match_index]
-
-	face_names.append(name)
-	print(face_names)
-
-	for (top, right, bottom, left), name in zip(face_locations, face_names):
-		# Draw a box around the face
-		cv2.rectangle(image2, (left, top), (right, bottom), (0, 0, 255), 2)
-
-		# Draw a label with a name below the face
-		cv2.rectangle(image2, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-		font = cv2.FONT_HERSHEY_DUPLEX
-		cv2.putText(image2, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-
-	img_to_show = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-	img_to_show2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
-
-	#stackowerflow
-	# img =  cv2.imread('2-d.jpg')
-	# cv2.namedWindow("test", cv2.WND_PROP_FULLSCREEN)          
-	# cv2.setWindowProperty("test", cv2.WND_PROP_FULLSCREEN, cv2.cv.CV_WINDOW_FULLSCREEN)
-	# cv2.imshow("test",img)
-
-	# Display the resulting image
-	# cv2.imshow('asd', img_to_show)
-
-	cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
-	cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)	
-	cv2.imshow('Video', img_to_show2)
-
-	cv2.waitKey(5000)
-	cv2.destroyAllWindows()
-
-
 def imagesFromDirectory(path="."):
 	files = [f for f in listdir(path) if isfile(join(path, f))]
 	return files
+
 
 def saveImage(src, dst):
 	dst_dir = dirname(dst)
@@ -96,6 +48,7 @@ def saveImage(src, dst):
 
 	print("copying image to {}".format(dst))
 	copy2(src, dst)
+	
 
 def initKnownFaces(path):
 	#TODO init known faces based on people directories if they exist
